8c1dff5d35b59dc2ce05f73068da21f29b933050
GabrielSavall <gabrielalejandro.savall@gmail.com>
2023-05-15 13:28:43 -0300

    Ultimas modificaciones

---
+++ src/components/ContainerItemCart/ContainerItemCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContainerItemCart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContainerItemCart
--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
+// Import Firebase
+
+import { db } from '../../firebaseConfig/firebaseConfig';
+import { collection, query, getDocs, where, documentId } from "firebase/firestore";
 
 // IMPORT CONTEXT
 
 import { CartContext } from '../../context/CartContext';
 
 const ItemCart = () => {
+    
+    //Trae los items del firebase
+    const q = query(collection(db,"items"));
+    const [item, setItem] = useState([]);
+
+    useEffect(()=> {
+      const getItems = async() => {
+        const querySnapshot = await getDocs(q);
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          docs.push({...doc.data(), id: doc.id})
+        });
+        setItem(docs)
+       
+      };
+  
+      getItems()
+  
+    }, [])
 
   // Trae el itemCart creado en el contexto para poder usarlo.
 
@@ -14,12 +38,8 @@ const ItemCart = () => {
   // Funcion quita del carrito
 
   const removeToCart = () => {
-    console.log(itemCart);
+
   }
-
-
-
-
 
 
 
@@ -33,12 +53,22 @@ const ItemCart = () => {
 
       {itemCart.map(data =>{
         return(
-            <div key={data.id}>
-              <p style={{marginLeft:'5px',marginTop:'10px' ,color:'green', fontWeight:'bold'}}> {data.name} | ${data.price} 
-                <button onClick={removeToCart} style={{marginBottom:'25px' ,marginLeft:'10px', height:'20px', width:'55px',background:'red',color:'white'}}>Quitar</button>             
-               </p> 
-              
-
+            <div key={data.id} style={{display:'flex', justifyContent:'center', marginBottom:'20px'}}>
+              <table>
+                <tr>
+                  <th style={{ backgroundColor:'black',color:'white',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>Producto</th>
+                  <th style={{ backgroundColor:'black',color:'white',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>Nombre</th>
+                  <th style={{ backgroundColor:'black',color:'white',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>Precio</th>
+                  <th style={{ backgroundColor:'black',color:'white',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>Total</th>
+                </tr>
+                <tr>
+                  <td><img src={data.img}alt={data.name} style={{width:'100px',border:'solid', borderColor:'black',borderWidth:'0.3px'}}></img></td>
+                  <td style={{color:'green',width:'200px', textAlign:'center',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>{data.name}</td>
+                  <td style={{color:'green',width:'200px',textAlign:'center',border:'solid', borderColor:'black',borderWidth:'0.3px'}}>{data.price}</td>
+                  <td style={{color:'green',width:'200px', textAlign: 'center',fontWeight:'bold', border:'solid', borderColor:'black',borderWidth:'0.3px'}}>{data.price}</td>
+                  <td><button onClick={removeToCart} style={{marginBottom:'25px' ,marginLeft:'10px', height:'20px', width:'55px',background:'red',color:'white'}}>Quitar</button></td>
+                </tr>
+              </table>
            </div>
         )
       })}
